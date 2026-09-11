28fb3bcf2ae4a3d5213f211f0e8ff046da6d6198
joaovctss <isweazg@gmail.com>
2026-09-11 10:18:43 -0300

    luiz

---
+++ attcasa.py
@@ -0,0 +1,7 @@
+#range 02/09/29#
+print(list(range(0,10,1)))
+print(list(range(10,0)))
+print(list(range(0,10,2)))
+print(list(range(10,0,-1)))
+print(list(range(10,0,-2)))
+print(list(range(10,0,3)))
