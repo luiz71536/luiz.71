db60b204a5d99fb08ff0945271898a3b1f5a2960
joaovctss <isweazg@gmail.com>
2026-09-11 10:20:52 -0300

    lgldfgkf

--- attcasa.py
+++ attcasa.py
@@ -1,7 +1,1 @@
-#range 02/09/29#
-print(list(range(0,10,1)))
-print(list(range(10,0)))
-print(list(range(0,10,2)))
-print(list(range(10,0,-1)))
-print(list(range(10,0,-2)))
-print(list(range(10,0,3)))
+print("luiz")
